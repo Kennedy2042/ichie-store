076f3e2a6653010a29c2c877fa392d989cfec82d
Kennedy2042 <ogbonnakennedy@gmail.com>
2026-05-09 19:40:32 +0100

    updated

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Container from '@/components/Container'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <Container className='bg-red-400'>Header</Container>
+    </div>
+  )
+}
+
+export default Header
